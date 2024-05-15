5358649dcea82e94c0c9c1aa9c479880d754f2e2
A.A Suvorov <smartlegiondev@gmail.com>
2024-05-15 10:56:16 +0700

    Fix date

--- climan/managers.py
+++ climan/managers.py
@@ -1,7 +1,7 @@
 # --------------------------------------------------------
 # Licensed under the terms of the BSD 3-Clause License
 # (see LICENSE for details).
-# Copyright © 2021, A.A Suvorov
+# Copyright © 2024, A.A Suvorov
 # All rights reserved.
 # --------------------------------------------------------
 # Url: https://github.com/smartlegionlab
